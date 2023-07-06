45e39e52e3cda626536819c71e17895357314328
Jabari G <grubbjabari@gmail.com>
2023-07-06 19:53:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ window=Tk()
 Button(window,text="New Window",command=create_window).pack()
 
 window.title('Virtual World')
-window.geometry("300x200")
+window.geometry("300x400")
 #TODO: Create a window that is the size of a phone or youtube shorts reel
 window.mainloop()
